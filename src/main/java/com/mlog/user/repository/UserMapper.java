8432c346402dfc3503f3e21bf23ffcf6cdcaee36
이지표 <shuaigejp@naver.com>
2024-05-03 16:30:12 +0900

    feat: h2 인메모리 데이터베이스를 초기화하는 sql 추가

--- src/main/java/com/mlog/user/repository/UserMapper.java
+++ src/main/java/com/mlog/user/repository/UserMapper.java
@@ -8,20 +8,20 @@ import java.util.Optional;
 @Mapper
 public interface UserMapper {
 
-    @Select("select * from user where email = #{email}")
+    @Select("select * from users where email = #{email}")
     Optional<User> findByEmail(String email);
 
-    @Select("select * from user where id = #{id}")
+    @Select("select * from users where id = #{id}")
     Optional<User> findById(Long id);
 
-    @Insert("insert into user (email, name, password, role, created_at, updated_at) " +
+    @Insert("insert into users (email, name, password, role, created_at, updated_at) " +
             "values (#{email}, #{name}, #{password}, #{role}, #{createdAt}, #{updatedAt}) ")
     void save(User user);
 
-    @Delete("delete from user where id = #{id}")
+    @Delete("delete from users where id = #{id}")
     void delete(Long id);
 
-    @Update("update user set email=#{email}, name=#{name}, password=#{password}, role=#{role}, updated_at=#{updatedAt} " +
+    @Update("update users set email=#{email}, name=#{name}, password=#{password}, role=#{role}, updated_at=#{updatedAt} " +
             "where id = #{id}")
     void update(User user);
 }
