f1d4bbfc1eea121315767a0fbdef034009edfc94
hellomatia <shuaigejp80@gmail.com>
2024-05-20 22:58:49 +0900

    feat: 모든 출처에 대해 CORS 허용

--- src/main/java/com/mlog/config/WebConfig.java
+++ src/main/java/com/mlog/config/WebConfig.java
@@ -10,8 +10,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로에 대해
-                .allowedOrigins("http://localhost:5173")
-                .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메소드
+                .allowedOrigins("*") // 모든 출처 허용
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 모든 HTTP 메소드 허용
+                .allowedHeaders("*") // 모든 헤더 허용
                 .allowCredentials(true); // 쿠키를 포함한 요청 허용
     }
 }
