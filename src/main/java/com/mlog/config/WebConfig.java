16718dd4d127a59a08f817ca6dbe4305eb0e3130
hellomatia <shuaigejp80@gmail.com>
2024-05-20 23:47:37 +0900

    fix: DNS URL 수정

--- src/main/java/com/mlog/config/WebConfig.java
+++ src/main/java/com/mlog/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로에 대해
-                .allowedOrigins("http://localhost:5173", "m-log.site")
+                .allowedOrigins("http://localhost:5173", "http://m-log.site")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 모든 HTTP 메소드 허용
                 .allowedHeaders("*") // 모든 헤더 허용
                 .allowCredentials(true); // 쿠키를 포함한 요청 허용
