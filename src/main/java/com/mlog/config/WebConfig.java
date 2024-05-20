e6010e4f43f0edae13b52a4e18c423ea2ce03e80
hellomatia <shuaigejp80@gmail.com>
2024-05-20 23:39:04 +0900

    feat: 로컬과 DNS URL에 대해 CORS 허용

--- src/main/java/com/mlog/config/WebConfig.java
+++ src/main/java/com/mlog/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로에 대해
-                .allowedOrigins("http://localhost:5173", "http://m-log-view.s3-website-ap-southeast-2.amazonaws.com")
+                .allowedOrigins("http://localhost:5173", "m-log.site")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 모든 HTTP 메소드 허용
                 .allowedHeaders("*") // 모든 헤더 허용
                 .allowCredentials(true); // 쿠키를 포함한 요청 허용
