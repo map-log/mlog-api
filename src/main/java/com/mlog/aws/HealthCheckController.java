9d01780a2f13e59fd26c8e9276fcf47c026610f9
이지표 <shuaigejp@naver.com>
2024-05-20 14:59:07 +0900

    feat: healthCheck 컨트롤러 추가

--- src/main/java/com/mlog/aws/HealthCheckController.java
+++ src/main/java/com/mlog/aws/HealthCheckController.java
@@ -5,12 +5,13 @@ import static com.mlog.util.ApiUtils.success;
 
 import com.mlog.util.ApiUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthCheckController {
 
-    @GetMapping("/health")
+    @RequestMapping("/health")
     public ApiResult<String> healthCheck() {
         return success("GOOD");
     }
