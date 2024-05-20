39ea0c6dd8d60da68ad335c229ba5a5b5b39e8f7
hellomatia <shuaigejp80@gmail.com>
2024-05-20 23:05:50 +0900

    feat: 로컬과 프런트 URL에 대해 CORS 허용

--- src/main/java/com/mlog/config/WebConfig.java
+++ src/main/java/com/mlog/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로에 대해
-                .allowedOrigins("*") // 모든 출처 허용
+                .allowedOrigins("http://localhost:5173", "http://m-log-view.s3-website-ap-southeast-2.amazonaws.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 모든 HTTP 메소드 허용
                 .allowedHeaders("*") // 모든 헤더 허용
                 .allowCredentials(true); // 쿠키를 포함한 요청 허용
